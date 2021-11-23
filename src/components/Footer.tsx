96fea0635c139f4f7479b1c0169e8a045c67f689
Sebastien Kempf <56154464+sebkpf@users.noreply.github.com>
2021-11-23 11:54:32 +0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,49 @@
 import { useRouter } from "next/router";
-import { HStack, Link } from "@chakra-ui/layout";
+import { Box, HStack, Link, VStack } from "@chakra-ui/layout";
 
 const Footer = () => {
   const router = useRouter();
 
   return (
-    <HStack as="footer" mt={12}>
-      <Link
-        variant={router.pathname === "/" && "brand"}
-        href="https://mobile.twitter.com/thetechpadawan"
-        isExternal
-      >
-        twitter
-      </Link>
-      <span>•</span>
-      <Link
-        variant={router.pathname === "/" && "brand"}
-        href="https://github.com/thetechpadawan"
-        isExternal
-      >
-        github
-      </Link>
-    </HStack>
+    <VStack as="footer" mt={12} align="flex-start">
+      <HStack>
+        <Link
+          variant={router.pathname === "/" && "brand"}
+          href="https://mobile.twitter.com/thetechpadawan"
+          isExternal
+        >
+          twitter
+        </Link>
+        <span>•</span>
+        <Link
+          variant={router.pathname === "/" && "brand"}
+          href="https://github.com/thetechpadawan"
+          isExternal
+        >
+          github
+        </Link>
+      </HStack>
+      <HStack w="full" justify="flex-end">
+        <Box>
+          Built by{" "}
+          <Link
+            variant={router.pathname === "/" && "brand"}
+            href="https://mobile.twitter.com/thetechpadawan"
+            isExternal
+          >
+            TechPadawan
+          </Link>{" "}
+          • Design inspired by{" "}
+          <Link
+            variant={router.pathname === "/" && "brand"}
+            href="https://overreacted.io/"
+            isExternal
+          >
+            Overreacted
+          </Link>
+        </Box>
+      </HStack>
+    </VStack>
   );
 };
 
