0c4cf5cb03f5b61bb9b19c74eedb5578c0304923
Sebastien Kempf <56154464+sebkpf@users.noreply.github.com>
2021-11-28 10:57:29 +0800

    add matomo tracking code
    update analytics tracking script
    
    update tracking code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Container, VStack } from "@chakra-ui/layout";
 import { Flex } from "@chakra-ui/react";
 import Header from "./Header";
@@ -5,14 +6,44 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <Container maxW="container.md" p={0}>
-      <Flex minH="100vh" direction="column" px={5} py={10}>
-        <Header />
-        <VStack as="main" flexGrow={1}>
-          {children}
-        </VStack>
-        <Footer />
-      </Flex>
-    </Container>
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+              _paq.push(["setCookieDomain", "*.techpadawan.com"]);
+              _paq.push(["setDomains", ["*.techpadawan.com"]]);
+              _paq.push(["setDoNotTrack", true]);
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//analytics.techpadawan.com/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+              `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img referrerpolicy="no-referrer-when-downgrade" src="https://analytics.techpadawan.com/matomo.php?idsite=1&amp;rec=1" style="border:0" alt="" />
+              `,
+          }}
+        />
+      </Head>
+      <Container maxW="container.md" p={0}>
+        <Flex minH="100vh" direction="column" px={5} py={10}>
+          <Header />
+          <VStack as="main" flexGrow={1}>
+            {children}
+          </VStack>
+          <Footer />
+        </Flex>
+      </Container>
+    </>
   );
 }
