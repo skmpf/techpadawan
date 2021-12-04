5d2dbcda7dc3df018a1b66ff270718516a4997a2
Sebastien Kempf <56154464+sebkpf@users.noreply.github.com>
2021-12-04 11:26:29 +0800

    update bio avatar
    update bio avatar

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,18 +1,18 @@
 import { useRouter } from "next/router";
 import { HStack, Text } from "@chakra-ui/layout";
-import { Image, Link } from "@chakra-ui/react";
+import { Img, Link } from "@chakra-ui/react";
 
 const Bio = () => {
   const router = useRouter();
 
   return (
     <HStack w="full" h="full" spacing={3} mb={8}>
-      {/* TODO replace image component by next image component for optimization */}
-      <Image
+      <Img
         borderRadius="full"
         boxSize="3.5rem"
-        src="https://bit.ly/sage-adebayo"
-        alt="Segun Adebayo"
+        src="https://res.cloudinary.com/dzzyt6yhh/image/upload/v1638592074/avatar_ykbmmm.png"
+        alt="TechPadawan avatar"
+        backgroundColor="brand.100"
       />
       <Text>
         Personal blog by{" "}
