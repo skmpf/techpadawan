622f30551cd4033ad22c5d48123546aa42f3a8b6
Sebastien Kempf <56154464+sebkpf@users.noreply.github.com>
2021-12-09 10:24:30 +0800

    render markdown

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -5,15 +5,9 @@ import ArticleDetails from "../../components/ArticleDetails";
 import { Box, Heading } from "@chakra-ui/layout";
 import { Link } from "@chakra-ui/react";
 import Bio from "../../components/Bio";
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import matter from "gray-matter";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -23,22 +17,43 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({ postData }) {
+export async function getStaticProps({ params }) {
+  const source = await getPostData(params.id);
+
+  const { content, data } = matter(source.fileContents);
+  const mdxSource = await serialize(content, { scope: data });
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+      fileContents: source.fileContents,
+    },
+  };
+}
+
+export default function Post({ source, frontMatter, fileContents }) {
   return (
     <>
       <Head>
-        <title>{postData.title} - TechPadawan</title>
+        <title>{frontMatter.title} - TechPadawan</title>
       </Head>
       <Box as="main" flexGrow={1}>
         <Box as="article">
           <Box as="header" mb={7}>
-            <Heading as="h1">{postData.title}</Heading>
+            <Heading as="h1">{frontMatter.title}</Heading>
             <ArticleDetails
-              date={postData.date}
-              fileContents={postData.contentHtml}
+              date={frontMatter.date}
+              fileContents={fileContents}
             />
           </Box>
-          <Box dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></Box>
+          <Box>
+            <MDXRemote {...source} />
+          </Box>
+          <Box mt={8}>
+            <NextLink href="/" passHref>
+              <Link textDecoration="none">← Back to home</Link>
+            </NextLink>
+          </Box>
         </Box>
       </Box>
       <Box as="aside" w="full" mt={24}>
